0dca1764b7ff7ff1418d35c9bfde4911f45d2243
kaur9923 <manveerkaurcan2002@gmail.com>
2020-10-05 18:35:05 -0400

    intial commit

---
+++ src/assignment2/Assignment2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+/**
+ *
+ * @author manvi jawanda
+ */
+public class Assignment2 {
+
+    private String suit;
+    private int value;
+
+    /**
+     * @param suit
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+   
+   
+    
+    
+}
